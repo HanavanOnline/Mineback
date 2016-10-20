f40aad807b676cd7517f3890b242e14152b98c8e
Tyler Hanavan <TylerHanavan@gmail.com>
2016-10-20 19:08:04 -0400

    Added MinebackEvent and MinebackKey to have plugin API

---
+++ Mineback/src/com/Mineback/plugin/MinebackEvent.java
@@ -0,0 +1,14 @@
+package com.Mineback.plugin;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class MinebackEvent extends Event {
+
+	@Override
+	public HandlerList getHandlers() {
+		
+		return null;
+	}
+
+}
---
+++ Mineback/src/com/Mineback/plugin/MinebackKey.java
@@ -0,0 +1,19 @@
+package com.Mineback.plugin;
+
+public class MinebackKey {
+	
+	private String keyName;
+
+	private MinebackKey(String keyName) {
+		
+		this.keyName = keyName;
+		
+	}
+	
+	public String getKeyName() {
+		
+		return this.keyName;
+		
+	}
+	
+}
