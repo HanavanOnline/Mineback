580d07b166b1b33e71ad5955ae5154dfb939dead
Tyler Hanavan <TylerHanavan@gmail.com>
2016-10-20 21:23:44 -0400

    Steps towards syncing kills with Mineback backend

--- Mineback/src/com/Mineback/plugin/PlayerListener.java
+++ Mineback/src/com/Mineback/plugin/PlayerListener.java
@@ -1,5 +1,48 @@
 package com.Mineback.plugin;
 
-public class PlayerListener {
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
+import com.Mineback.plugin.data.types.KillData;
+
+public class PlayerListener implements Listener {
+
+	private Mineback mineback = null;
+
+	public PlayerListener(Mineback mineback) {
+		
+		this.mineback = mineback;
+		
+	}
+	
+	@EventHandler
+	public void onPlayerKillPlayer(PlayerDeathEvent event) {
+		
+		Player victim = event.getEntity();
+		
+		if(victim == null)
+			return;
+		
+		if(victim.getKiller() != null) {
+			
+			Player killer = victim.getKiller();
+			
+			KillData data = new KillData(this.getMineback());
+			
+			data.setKiller(killer).setVictim(victim);
+			
+			this.mineback.getDataQueue().addData(data);
+			
+		}
+		
+	}
+	
+	public Mineback getMineback() {
+		
+		return this.mineback;
+		
+	}
+	
 }
--- Mineback/src/com/Mineback/plugin/data/types/KillData.java
+++ Mineback/src/com/Mineback/plugin/data/types/KillData.java
@@ -1,13 +1,50 @@
 package com.Mineback.plugin.data.types;
 
+import org.bukkit.entity.Player;
+
 import com.Mineback.plugin.Mineback;
 import com.Mineback.plugin.data.MinebackData;
 import com.Mineback.plugin.data.RequestType;
 
 public class KillData extends MinebackData {
+	
+	private String killerName = null;
+	private String victimName = null;
 
 	public KillData(Mineback mineback) {
 		super(mineback);
+		
+	}
+	
+	public KillData setKiller(Player killer) {
+		
+		this.getData().put("killer", killer.getName());
+		
+		this.killerName = killer.getName();
+		
+		return this;
+		
+	}
+	
+	public String getKillerName() {
+		
+		return this.killerName;
+		
+	}
+	
+	public KillData setVictim(Player victim) {
+		
+		this.getData().put("victim", victim.getName());
+		
+		this.victimName = victim.getName();
+		
+		return this;
+		
+	}
+	
+	public String getVictimName() {
+		
+		return this.victimName;
 		
 	}
 
